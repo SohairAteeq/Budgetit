cc8746d68ae9c8325c1c357299295e6fb9a0897e
SohairAteeq <125694563+SohairAteeq@users.noreply.github.com>
2025-09-28 15:30:40 +0500

    fixed env

--- frontend/src/util/ApiEndPoints.js
+++ frontend/src/util/ApiEndPoints.js
@@ -1,25 +1,25 @@
-export const baseUrl = "https://budgetit-qft3.onrender.com/api/v1.0";
-export const CLOUDINARY_CLOUD_NAME = "dfnj9sbrz"
-export const CLOUDINARY_CLOUD_UNSIGNED_PRESET = "Budgetit"
+export const baseUrl = import.meta.env.VITE_API_BASE_URL;
+export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_CLOUD_UNSIGNED_PRESET = import.meta.env.VITE_CLOUDINARY_PRESET;
 
 export const ApiEndPoints = {
-    login: "/profile/login",
-    register: "/profile/register",
-    activate: "/activate",
-    authCheck: "/profile/getProfile",
-    dashboard: "/dashboard",
-    // Categories
-    categories: "/categories",
-    categoryByType: (type) => `/categories/${type}`,
-    updateCategory: (id) => `/categories/${id}`,
-    // Incomes
-    incomes: "/incomes",
-    deleteIncome: (incomeId) => `/incomes?incomeId=${incomeId}`,
-    // Expenses
-    expenses: "/expenses",
-    deleteExpense: (expenseId) => `/expenses?expenseId=${expenseId}`,
-    // Filters
-    filters: "/filters/filter",
-    // Cloudinary
-    UPLOAD_IMAGE: "https://api.cloudinary.com/v1_1/dfnj9sbrz/image/upload" 
+  login: "/profile/login",
+  register: "/profile/register",
+  activate: "/activate",
+  authCheck: "/profile/getProfile",
+  dashboard: "/dashboard",
+  // Categories
+  categories: "/categories",
+  categoryByType: (type) => `/categories/${type}`,
+  updateCategory: (id) => `/categories/${id}`,
+  // Incomes
+  incomes: "/incomes",
+  deleteIncome: (incomeId) => `/incomes?incomeId=${incomeId}`,
+  // Expenses
+  expenses: "/expenses",
+  deleteExpense: (expenseId) => `/expenses?expenseId=${expenseId}`,
+  // Filters
+  filters: "/filters/filter",
+  // Cloudinary
+  UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`
 };
